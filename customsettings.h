945c20ef14efe532f4c6f2290e21c6baf73313e7
lepickle <mark_jethro27@yahoo.com>
2015-11-14 21:34:11 +0800

    Add import for mainwindow.h

--- customsettings.h
+++ customsettings.h
@@ -2,6 +2,7 @@
 #define CUSTOMSETTINGS_H
 
 #include <QDialog>
+#include "mainwindow.h"
 
 namespace Ui {
 class CustomSettings;
@@ -12,8 +13,14 @@ class CustomSettings : public QDialog
     Q_OBJECT
 
 public:
+    MainWindow *mainWindowForm;
     explicit CustomSettings(QWidget *parent = 0);
     ~CustomSettings();
+
+private slots:
+    void on_btn_Cancel_clicked();
+
+    void on_btn_AddRegex_clicked();
 
 private:
     Ui::CustomSettings *ui;
