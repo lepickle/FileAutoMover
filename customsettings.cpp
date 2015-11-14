04386755fa5fe8965984830d0393a7ce44795b3e
lepickle <mark_jethro27@yahoo.com>
2015-11-14 21:34:33 +0800

    Import stringlist and model

--- customsettings.cpp
+++ customsettings.cpp
@@ -1,5 +1,10 @@
 #include "customsettings.h"
 #include "ui_customsettings.h"
+#include <QStringListModel>
+#include <QStringList>
+
+QStringListModel *model;
+QStringList list;
 
 CustomSettings::CustomSettings(QWidget *parent) :
     QDialog(parent),
@@ -11,4 +16,24 @@ CustomSettings::CustomSettings(QWidget *parent) :
 CustomSettings::~CustomSettings()
 {
     delete ui;
+}
+
+void CustomSettings::on_btn_Cancel_clicked()
+{
+    mainWindowForm->show();
+    this->hide();
+}
+
+/*TODO:
+ * Allow Deletion from list (hint: get index of list then compare to list)
+ * Return the list of items from child to Parent
+ * Make those shit work
+ */
+
+void CustomSettings::on_btn_AddRegex_clicked()
+{
+    model = new QStringListModel(this);
+    list.append(ui->txt_Regex->text());
+    model->setStringList(list);
+    ui->listView->setModel(model);
 }
